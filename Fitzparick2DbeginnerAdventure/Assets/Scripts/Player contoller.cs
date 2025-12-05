9199e69a22ee84b9f3e5c34c925891b535c0e5b2
736023-arch <736023@student.dusd.net>
2025-12-05 15:30:09 -0800

    coliders
    finshed the box coliders

--- Fitzparick2DbeginnerAdventure/Assets/Scripts/Player contoller.cs
+++ Fitzparick2DbeginnerAdventure/Assets/Scripts/Player contoller.cs
@@ -1,27 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-public class PlayerControllerTutorialUpdates : MonoBehaviour
+
+public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d; 
     float horizontal;
     float vertical;
 
     // Start is called before the first frame update
-    void Start()
+
+void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
-        // Update is called once per frame
-        void Update()
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal"); vertical = Input.GetAxis("Vertical");
+    }
+
+void FixedUpdate()
         {
-             horizontal = Input.GetAxis("Horizontal");
-             vertical = Input.GetAxis("Vertical");
+            Vector2 position= rigidbody2d.position;
+            position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-
-
-            void 
+        rigidbody2d.MovePosition(position);
         }
-    
+     
 }
